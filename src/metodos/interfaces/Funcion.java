da42a2183f01383bf954f1d0c454785cfd3c06a1
AlexAvVilla <alex.avila3232@gmail.com>
2023-05-07 17:46:22 -0600

    Se añade el programa

---
+++ src/metodos/interfaces/Funcion.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package metodos.interfaces;
+
+import javax.swing.JOptionPane;
+import jme.Expresion;
+import jme.excepciones.ExpresionException;
+
+/**
+ *
+ * @author Alex
+ */
+public class Funcion implements Ifuncion{
+    private String definicion = "";
+    Expresion expresion;
+
+    public Funcion(String definicion) {
+        this.definicion = definicion;        
+    }                        
+    
+    public boolean contieneX(String cad) {
+        for (int i = 0; i < cad.length(); i++) {
+            if (cad.substring(i, i + 1).equals("x")) 
+                return true;                
+        }
+        return false;
+    }
+    
+    
+    @Override
+    public double eval(double x) {
+        try {
+            expresion = new Expresion(this.definicion);            
+            expresion.setVariable("x", x);
+            return Double.parseDouble(String.valueOf(expresion.evaluar()));
+        } catch (ExpresionException ex) {
+            JOptionPane.showMessageDialog(null, "Error en la función.", "Error", JOptionPane.ERROR_MESSAGE);
+            return Double.NaN; 
+        }
+    }
+    
+}
